f912179d84972d89bff698257a62ef1a7699e48a
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-20 14:40:21 -0300

    Feat: Adiciona variáveis de ambiente.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { app } from './app';
+import { env } from "./env";
 
 app.listen({
-  port: 3000,
+  // port: 3000,
+  port: env.PORT,
   host: '0.0.0.0',
 }).then(() => {
   console.log('Server started');
